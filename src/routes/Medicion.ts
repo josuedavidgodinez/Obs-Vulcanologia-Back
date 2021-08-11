222fa0b0130b158b6b27c0bf9e5b9dcae202edf0
David Godinez <godinezjosue@hotmail.com>
2021-08-11 01:21:20 -0600

    detalles consulta en DB

--- src/routes/Medicion.ts
+++ src/routes/Medicion.ts
@@ -5,13 +5,21 @@ const medicion =  Router();
 
 
 medicion.get('/', function(req,  res) {
+   const fecha_i = req.body.fecha_i;
+   const fecha_f = req.body.fecha_f;
+   if(!fecha_i)
+   return res.status(400).json({ message : "Error no se proporciono fecha_i"});
    
-    pool.query('select * from metrics',(err,data)=>{
+   if(!fecha_f)
+   return res.status(400).json({ message : "Error no se proporciono fecha_f"});
+
+   const query = "select * from metrics where tiempo between '" + fecha_i + "' and '" +fecha_f+"'" 
+    pool.query(query,(err,data)=>{
         //pool.end()
         if(err){
         return res.status(500).json({ message : err});}
        
-        return res.status(200).json(data);       
+        return res.status(200).json(data.rows);       
     })
 });
 
