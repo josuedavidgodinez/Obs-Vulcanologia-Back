7f9d4022f874d5620b4d1671006aa98082e92adf
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2021-08-11 16:11:01 -0600

    Modificar Utilidades

--- src/models/statusCode.ts
+++ src/models/statusCode.ts
@@ -2,12 +2,17 @@ export enum statusCode{
     ok = 200,
     created = 201,
     accepted = 202,
+    nonAuthoritativeInformation = 203,
     noContent = 204,
     resetContent = 205,
+    partialContent = 206,
     badRequest = 400,
     unauthorized = 401,
     forbidden = 403,
     notFound = 404,
+    methodNotAllowed = 405,
+    notAcceptable = 406,
+    requestTimeout = 408,
     conflict = 409,
     ImaTeapot = 418
 };
