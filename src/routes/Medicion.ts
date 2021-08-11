a8f4d924d269cb88ed61faa771940bb95496dd8a
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2021-08-11 16:16:51 -0600

    Update Medicion.ts
    Corrección de forma de acceso a parámetros. Uso de Utilidades.

--- src/routes/Medicion.ts
+++ src/routes/Medicion.ts
@@ -1,26 +1,45 @@
-  
 import { Router } from 'express';
-import pool  from '../database/db';
-const medicion =  Router();
+import pool from '../database/db';
+import { statusCode } from "../models/statusCode";
+const med: Router =  Router();
 
-
-medicion.get('/', function(req,  res) {
-   const fecha_i = req.body.fecha_i;
-   const fecha_f = req.body.fecha_f;
-   if(!fecha_i)
-   return res.status(400).json({ message : "Error no se proporciono fecha_i"});
-   
-   if(!fecha_f)
-   return res.status(400).json({ message : "Error no se proporciono fecha_f"});
-
-   const query = "select * from metrics where tiempo between '" + fecha_i + "' and '" +fecha_f+"'" 
-    pool.query(query,(err,data)=>{
-        //pool.end()
-        if(err){
-        return res.status(500).json({ message : err});}
-       
-        return res.status(200).json(data.rows);       
-    })
+// host/med?fecha_i=_fechaInicio&fecha_f=_fechaFin
+med.get('/', function(req,  res) {
+    const url_query: any = req.query;
+    const fecha_i = url_query.fecha_i;
+    const fecha_f = url_query.fecha_f;
+    if (!fecha_i) {
+        res.status(statusCode.badRequest)
+        .json({
+            message : "Error no se proporciono fecha_i",
+            status: statusCode.badRequest
+        });
+        return;
+    }  
+    if (!fecha_f) {
+        res.status(statusCode.badRequest)
+        .json({
+            message : "Error no se proporciono fecha_f",
+            status: statusCode.badRequest
+        });
+        return;
+    }
+    const query = "select * from metrics where tiempo between '" + fecha_i + "' and '" +fecha_f+"'" 
+    pool.query(query, (err,data) => {
+        if (err) {
+            res.status(statusCode.conflict)
+            .json({
+                message : err,
+                status: statusCode.conflict
+            })
+            return;
+        }
+        res.status(statusCode.ok)
+        .json({
+            data: data.rows,
+            status: statusCode.ok
+        });
+    });
 });
 
-export default medicion;
+export default med;
