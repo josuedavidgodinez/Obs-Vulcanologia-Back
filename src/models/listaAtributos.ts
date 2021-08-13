3a54c9c39827b6a32835b8f8828dad6ca939f769
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2021-08-13 01:15:40 -0600

    Create listaAtributos.ts
    Lista de atributos de la tabla que contiene las mediciones

---
+++ src/models/listaAtributos.ts
@@ -0,0 +1,12 @@
+export const listaAtributos = {
+    acelerometrox: 'acelerometrox',
+    acelerometroy: 'acelerometroy',
+    acelerometroz: 'acelerometroz',
+    fecha: 'fecha',
+    fechaguardado: 'fechaguardado',
+    infrasonido1: 'infrasonido1',
+    infrasonido2: 'infrasonido2',
+    infrasonido3: 'infrasonido3',
+    infrasonido4: 'infrasonido4',
+    medicion_id: 'medicion_id'
+}
