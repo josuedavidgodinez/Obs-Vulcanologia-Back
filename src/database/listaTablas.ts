665ab4565a817bbe03c68f67960a56c7c208b406
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2021-08-26 19:33:00 -0600

    Create listaTablas.ts

---
+++ src/database/listaTablas.ts
@@ -0,0 +1,5 @@
+const lista: { [id: string] : string; } = {};
+lista['e1ms1'] = 'polls_e1ms1';
+lista['ise1'] = 'polls_ise1_infra';
+lista['ise2'] = 'polls_ise2_infra';
+export const listaTablas = lista;
