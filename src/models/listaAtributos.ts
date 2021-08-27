eb8c59d308b5d45c781d0abffd4e260ca0df60f0
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2021-08-26 19:32:57 -0600

    Update listaAtributos.ts

--- src/models/listaAtributos.ts
+++ src/models/listaAtributos.ts
@@ -1,12 +1,11 @@
 export const listaAtributos = {
-    acelerometrox: 'acelerometrox',
-    acelerometroy: 'acelerometroy',
-    acelerometroz: 'acelerometroz',
-    fecha: 'fecha',
-    fechaguardado: 'fechaguardado',
-    infrasonido1: 'infrasonido1',
-    infrasonido2: 'infrasonido2',
-    infrasonido3: 'infrasonido3',
-    infrasonido4: 'infrasonido4',
-    medicion_id: 'medicion_id'
+    acelerometrox: 'mpu_axe',
+    acelerometroy: 'mpu_aye',
+    acelerometroz: 'mpu_aze',
+    fecha: 'fecha_sistema',
+    fechaguardado: 'fecha_recepcion',
+    infrasonido1: 'infrasonido_1',
+    infrasonido2: 'infrasonido_2',
+    infrasonido3: 'infrasonido_3',
+    infrasonido4: 'infrasonido_4'
 }
