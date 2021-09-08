5598a5dac265d2e8514a24df4fbe0b1ada761340
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2021-09-08 15:12:23 -0600

    Update listaTablas.ts

--- src/database/listaTablas.ts
+++ src/database/listaTablas.ts
@@ -3,3 +3,9 @@ lista['e1ms1'] = 'polls_e1ms1';
 lista['ise1'] = 'polls_ise1_infra';
 lista['ise2'] = 'polls_ise2_infra';
 export const listaTablas = lista;
+
+const estaciones: { [id: string] : number; } = {};
+estaciones['e1ms1'] = 3;
+estaciones['ise1'] = 1;
+estaciones['ise2'] = 2;
+export const listaEstaciones = estaciones;
