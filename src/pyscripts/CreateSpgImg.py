1ae7ffe771c97d1aa6708c0c43c182076e5aab76
David Godinez <godinezjosue@hotmail.com>
2021-10-07 23:06:46 -0600

    script en py para generar espectrograma

---
+++ src/pyscripts/CreateSpgImg.py
@@ -0,0 +1,29 @@
+from __future__ import print_function, with_statement
+#from  obspy import read
+import os
+import numpy as np
+import pandas as pd
+from datetime import datetime, timedelta
+from obspy import UTCDateTime, read, Trace, Stream
+
+# imgPath = sys.argv[1]
+# filePath = sys.argv[2]
+
+imgPath = '/home/ubuntu/Downloads/tempData/prueba1.png'
+filePath = '/home/ubuntu/Downloads/tempData/prueba.txt'
+
+txt=''
+with open(filePath,'r') as f:
+    txt=f.read()
+miniseeds=txt.split('\n')
+st=read(miniseeds[0])
+if(len(miniseeds)>1):
+    for i in range (1,len(miniseeds)):
+        if len(miniseeds[i]) > 0:
+            st+=read(miniseeds[i])
+
+import matplotlib
+matplotlib.use('Agg')
+
+st.spectrogram(log=True, title='Espectrograma', outfile=imgPath)
+os.remove(filePath)
