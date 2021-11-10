1a3e490fe3941e2be2fb7f479d86e71509933614
Javier Mollinedo <35619690+JMollinedo@users.noreply.github.com>
2021-11-10 10:12:58 -0600

    Update GetPhoto.ts
    Update Query

--- src/services/GetPhoto.ts
+++ src/services/GetPhoto.ts
@@ -4,8 +4,11 @@ import pool from "../database/dbFotos";
  * @returns devuelve la ruta de la última imagen del volcán 
  */
 export const getLastPhoto = async(): Promise<string> => {
-    const query = 'SELECT path FROM filecatalog ORDER BY fecha DESC LIMIT 1';
+    const c = "'";
+    const name = c + 'final.jpg' + c;
+    const query = 'SELECT path FROM filecatalog WHERE originalname = ' + name + ' ORDER BY fecha DESC LIMIT 1';
     const query_result = await pool.query(query);
+    if(query_result.rowCount == 0) throw new Error("Image not found");
     const imgPath = query_result.rows[0].path;
     return imgPath;
 }
